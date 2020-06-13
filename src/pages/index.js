8d3305876f631e80961a4064e66dc588117c0cc8
Nikolay Lechev <nikolay@kollegorna.se>
2020-06-13 23:16:14 +0300

    Init hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,33 @@ import React from "react"
 import styled from "styled-components"
 // import Img from "gatsby-image"
 
+import { rem } from "polished"
 // import { graphql } from "gatsby"
 
 const Container = styled.section.attrs({ className: `styled` })`
-  overflow: hidden;
+  background: ${props => props.theme.colorBlack};
+`
+
+const ContainerInner = styled.div`
   min-height: 100vh;
   height: 100vh;
-  background: ${props => props.theme.colorBlack};
+  ${props => props.theme.gridContainer()};
   color: ${props => props.theme.colorWhite};
+  padding-top: ${rem(100)};
+`
+
+const Content = styled.div`
+  ${props => props.theme.gridCell(8)};
 `
 
 export default ({ data }) => (
   <Container>
-    <h1>Hello Gatsby</h1>
-    <p>What a world.</p>
+    <ContainerInner>
+      <Content>
+        <h1>Try and test. Often.</h1>
+        <p>This is a digital playground for Kollegorna experiments.</p>
+      </Content>
+    </ContainerInner>
     {/* <Img fluid={data.imgMain.childImageSharp.fluid} /> */}
   </Container>
 )
