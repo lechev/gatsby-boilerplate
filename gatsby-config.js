e1b1c3dfc9397acdbad4279503320b4ec9b6e469
Nikolay Lechev <nikolay@kollegorna.se>
2020-07-18 19:13:12 +0300

    Add site meta information

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,61 @@
  */
 const path = require(`path`)
 
+const lang = `en`
+const name = `Playground`
+const shortName = `Playground`
+const title = `Try and Test`
+const titlePattern = `[PAGE_TITLE] · [SITE_NAME]`
+const description = `This is a digital playground for personal experiments.`
+const colorMain = `#2a2b32`
+const colorBg = `#f2f2f2`
+const socialImage = `/meta-images/social.jpg`
+const icon = `/meta-images/icon-transparent.png` // favicon, ms tile
+const iconBgColor = colorBg
+const iconWithBg = `/meta-images/icon-with-bg.png` // apple touch, webmanifest
+const favIcon = icon
+const maskIcon = `/meta-images/mask-icon.svg` // https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/pinnedTabs/pinnedTabs.html
+const maskIconColor = colorMain
+const fbAppId = ``
+const twitterHandle = `@iamlechev`
+const robotsNoFollow = false
+const robotsNoIndex = false
+
 module.exports = {
   siteMetadata: {
-    title: "Try and Test",
-    titleTemplate: "%s · Playground",
-    description: "This is a digital playground for personal experiments.",
+    lang,
+    name,
+    title,
+    titlePattern,
+    description,
+    socialImage,
+    fbAppId,
+    twitterHandle,
+    icon,
+    iconBgColor,
+    favIcon,
+    maskIcon,
+    maskIconColor,
+    robotsNoFollow,
+    robotsNoIndex,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        lang: lang,
+        name: name,
+        short_name: shortName,
+        description: description,
+        theme_color: colorMain,
+        background_color: colorBg,
+        icon: `static${iconWithBg}`,
+        include_favicon: false, // carried out via `components/meta` instead
+        start_url: `/`,
+        display: `minimal-ui`,
+        cache_busting_mode: `name`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
