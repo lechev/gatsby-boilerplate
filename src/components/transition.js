848edcf8b90ce8d8c4bf75cabfb42c5fa2abbb92
Nikolay Lechev <nikolay@kollegorna.se>
2020-07-18 19:54:05 +0300

    Hotfix/meta (#5)
    * Add site meta information
    
    * Add offline plugin and missing mask icon

--- src/components/transition.js
+++ src/components/transition.js
@@ -4,7 +4,7 @@ import {
   Transition as ReactTransition,
 } from "react-transition-group"
 
-const timeout = 250
+const timeout = 300
 const getTransitionStyles = {
   entering: {
     opacity: 0.5,
